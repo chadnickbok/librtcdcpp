d8663b235738ec0324d67af2b2df64409547757e
Nick Chadwick <chadnickbok@gmail.com>
2016-07-27 22:15:05 -0700

    Working, much-simpler WebSocket client

---
+++ examples/websocket_client/WebSocketWrapper.cpp
@@ -0,0 +1,61 @@
+#include "WebSocketWrapper.hpp"
+
+#include <iostream>
+
+WebSocketWrapper::WebSocketWrapper(std::string url) :
+  url(url),
+  send_queue()
+{
+  ;
+}
+
+WebSocketWrapper::~WebSocketWrapper()
+{
+  ;
+}
+
+bool WebSocketWrapper::Initialize()
+{
+  this->ws = WebSocket::from_url(this->url);
+  return this->ws ? true : false;
+}
+
+void WebSocketWrapper::SetOnMessage(std::function<void(std::string)> onMessage) {
+  this->onMessage = onMessage;
+}
+
+void WebSocketWrapper::Start()
+{
+  this->send_loop = std::thread(&WebSocketWrapper::Loop, this);
+}
+
+void WebSocketWrapper::Loop()
+{
+  this->ws->send("hello");
+  while (true) {
+    this->ws->poll();
+
+    if (!this->send_queue.empty()) {
+      ChunkPtr chunk = this->send_queue.wait_and_pop();
+      std::cout << "send msg of length " << chunk->Length() << "\n";
+      std::string msg(reinterpret_cast< char const* >(chunk->Data()), chunk->Length());
+      this->ws->send(msg);
+      this->ws->poll();
+    }
+    this->ws->dispatch(this->onMessage);
+  }
+}
+
+void WebSocketWrapper::Send(std::string msg) {
+  this->send_queue.push(
+    std::shared_ptr<Chunk>(
+      new Chunk(
+        (const void *) msg.c_str(), msg.length()
+      )
+    )
+  );
+}
+
+void WebSocketWrapper::Close() {
+  ;
+}
---
+++ examples/websocket_client/WebSocketWrapper.hpp
@@ -0,0 +1,44 @@
+/**
+ * Simple libwebsockets C++ wrapper
+ */
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <thread>
+#include <memory>
+#include <mutex>
+
+#include "easywsclient.hpp"
+
+#include <assert.h>
+#include <stdio.h>
+#include <string>
+
+#include "../../include/ChunkQueue.hpp"
+
+using easywsclient::WebSocket;
+
+class WebSocketWrapper {
+public:
+  WebSocketWrapper(std::string url);
+  virtual ~WebSocketWrapper();
+
+  bool Initialize();
+  void Start();
+  void Send(std::string);
+  void Close();
+
+  void SetOnMessage(std::function<void(std::string)>);
+  void SetOnClose(std::function<void()>);
+  void SetOnError(std::function<void(std::string)>);
+
+private:
+  void Loop();
+
+  WebSocket::pointer ws;
+  std::string url;
+  ChunkQueue send_queue;
+  std::function<void(std::string)> onMessage;
+  std::thread send_loop;
+};
